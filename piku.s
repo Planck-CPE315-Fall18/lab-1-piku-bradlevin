890e2291a8c49bc767de1a5f945964415ddb47a1
Bradley Levin <bmlevin@pi06.csc.calpoly.edu>
2018-09-25 21:50:09 +0000

    Final Revision

--- piku.s
+++ piku.s
@@ -8,7 +8,11 @@ main:
     push    {ip, lr}
 
     @ Load the argument and perform the call. This is like 'printf("...")' in C.
-    ldr     r0, =message
+    ldr     r0, =message1
+    bl      printf
+    ldr     r0, =message2
+    bl      printf
+    ldr     r0, =message3
     bl      printf
 
     @ Exit from 'main'. This is like 'return 0' in C.
@@ -20,5 +24,9 @@ main:
     @ --------------------------------
     @ Data for the printf call. The GNU assembler's ".asciz" directive
     @ automatically adds a NULL character termination.
-message:
-    .asciz  "Hello, world.\n"
+message1:
+    .asciz  "Powerful and small\n"
+message2:
+    .asciz  "Fourty pins and lots of chips\n"
+message3:
+    .asciz  "My Raspberry Pi\n"
